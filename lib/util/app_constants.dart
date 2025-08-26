59d0221ef648f78f44ebbb3cdc4680ac032a2e86
Hafiz <21999345+AhmadHafizzudin@users.noreply.github.com>
2025-08-26 17:21:03 +0800

    Update app_constants.dart

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -8,10 +8,10 @@ class AppConstants {
 
   static const String fontFamily = 'Roboto';
   static const bool payInWevView = false;
-  static const String webHostedUrl = 'https://stackfood-web.6amtech.com';
+  static const String webHostedUrl = 'https://kawanku.app';
   static const bool useReactWebsite = false;
 
-  static const String baseUrl = 'https://kawanku.app';
+  static const String baseUrl = 'https://admin.kawanku.app';
   static const String categoryUri = '/api/v1/categories';
   static const String bannerUri = '/api/v1/banners';
   static const String restaurantProductUri = '/api/v1/products/latest';
