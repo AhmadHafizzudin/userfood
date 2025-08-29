0604e660d9f8676b38b2a4c725bff4eeee540185
Hafiz <21999345+AhmadHafizzudin@users.noreply.github.com>
2025-08-29 10:40:36 +0800

    Update custom_image_widget.dart

--- lib/common/widgets/custom_image_widget.dart
+++ lib/common/widgets/custom_image_widget.dart
@@ -1,9 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:stackfood_multivendor/common/widgets/custom_asset_image_widget.dart';
 import 'package:stackfood_multivendor/util/app_constants.dart';
 import 'package:stackfood_multivendor/util/images.dart';
-import 'package:flutter/material.dart';
 
 class CustomImageWidget extends StatefulWidget {
   final String image;
@@ -15,8 +15,19 @@ class CustomImageWidget extends StatefulWidget {
   final bool isRestaurant;
   final bool isFood;
   final Color? color;
-  const CustomImageWidget({super.key, required this.image, this.height, this.width, this.fit = BoxFit.cover, this.placeholder = '', this.imageColor,
-    this.isRestaurant = false, this.isFood = false, this.color});
+
+  const CustomImageWidget({
+    super.key,
+    required this.image,
+    this.height,
+    this.width,
+    this.fit = BoxFit.cover,
+    this.placeholder = '',
+    this.imageColor,
+    this.isRestaurant = false,
+    this.isFood = false,
+    this.color,
+  });
 
   @override
   State<CustomImageWidget> createState() => _CustomImageWidgetState();
@@ -27,29 +38,58 @@ class _CustomImageWidgetState extends State<CustomImageWidget> {
 
   @override
   Widget build(BuildContext context) {
+    final bool hasImage = widget.image.isNotEmpty;
+
+    final String resolvedImageUrl = hasImage
+        ? (kIsWeb
+            ? '${AppConstants.baseUrl}/image-proxy?url=${Uri.encodeComponent(widget.image)}'
+            : widget.image)
+        : '';
+
+    final String fallbackImage = widget.placeholder.isNotEmpty
+        ? widget.placeholder
+        : widget.isRestaurant
+            ? Images.restaurantPlaceholder
+            : widget.isFood
+                ? Images.foodPlaceholder
+                : Images.placeholderPng;
+
     return MouseRegion(
-      onEnter: (_) {
-        setState(() {
-          _isHovered = true;
-        });
-      },
-      onExit: (_) {
-        setState(() {
-          _isHovered = false;
-        });
-      },
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
       child: AnimatedScale(
         scale: _isHovered ? 1.2 : 1.0,
         duration: Duration(milliseconds: 300),
         curve: Curves.easeInOut,
-        child: CachedNetworkImage(
-          color: widget.color,
-          imageUrl: kIsWeb ? '${AppConstants.baseUrl}/image-proxy?url=${widget.image}' : widget.image, height: widget.height, width: widget.width, fit: widget.fit,
-          placeholder: (context, url) => CustomAssetImageWidget(widget.placeholder.isNotEmpty ? widget.placeholder : widget.isRestaurant ? Images.restaurantPlaceholder : widget.isFood ? Images.foodPlaceholder : Images.placeholderPng,
-              height: widget.height, width: widget.width, fit: widget.fit, color: widget.imageColor),
-          errorWidget: (context, url, error) => CustomAssetImageWidget(widget.placeholder.isNotEmpty ? widget.placeholder : widget.isRestaurant ? Images.restaurantPlaceholder : widget.isFood ? Images.foodPlaceholder : Images.placeholderPng,
-              height: widget.height, width: widget.width, fit: widget.fit, color: widget.imageColor),
-        ),
+        child: hasImage
+            ? CachedNetworkImage(
+                imageUrl: resolvedImageUrl,
+                height: widget.height,
+                width: widget.width,
+                fit: widget.fit,
+                color: widget.color,
+                placeholder: (context, url) => CustomAssetImageWidget(
+                  fallbackImage,
+                  height: widget.height,
+                  width: widget.width,
+                  fit: widget.fit,
+                  color: widget.imageColor,
+                ),
+                errorWidget: (context, url, error) => CustomAssetImageWidget(
+                  fallbackImage,
+                  height: widget.height,
+                  width: widget.width,
+                  fit: widget.fit,
+                  color: widget.imageColor,
+                ),
+              )
+            : CustomAssetImageWidget(
+                fallbackImage,
+                height: widget.height,
+                width: widget.width,
+                fit: widget.fit,
+                color: widget.imageColor,
+              ),
       ),
     );
   }
