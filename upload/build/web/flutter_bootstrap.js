48f391925c4ddc69f7ebb70cbe12e8822eb308fd
Hafiz <21999345+AhmadHafizzudin@users.noreply.github.com>
2025-09-25 13:35:39 +0800

    new

--- upload/build/web/flutter_bootstrap.js
+++ upload/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "838941818"
+    serviceWorkerVersion: "3571343250"
   }
 });
